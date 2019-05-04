c9cc1b071192cd6c0a7f46361976f527dbffa53a
yangqiong <yangqionggo@163.com>
2019-05-04 15:48:22 +0800

    4.2.3 有向图的可达性

---
+++ algs4/04_chapter/DirectedDFS.java
@@ -0,0 +1,52 @@
+/** 
+ * 4.2.3 有向图的可达性
+ * 编译：javac -cp :../stdlib.jar:../.:../01_chapter/ DirectedDFS.java
+ * 运行：java -cp :../stdlib.jar:../.:../01_chapter/ DirectedDFS.java tinyDG.txt 2
+ */
+
+
+public class DirectedDFS {
+    private boolean[] marked;
+
+    public DirectedDFS(Digraph G, int s){
+        marked = new boolean[G.V()];
+        dfs(G, s);
+    }
+
+    public DirectedDFS(Digraph G, Iterable<Integer> sources){
+        marked = new boolean[G.V()];
+        for (int s : sources){
+            if (!marked[s]) {
+                dfs(G, s);
+            }
+        }
+    }
+
+    private void dfs(Digraph G, int v){
+        marked[v] = true;
+        for (int w : G.adj(v)){
+            if (!marked[w]){
+                dfs(G, w);
+            }
+        }
+    }
+
+    public boolean marked(int v){
+        return marked[v];
+    }
+
+    public static void main(String[] args){
+        Digraph G = new Digraph(new In(args[0]));
+        Bag<Integer> sources = new Bag<Integer>();
+        for (int i = 1; i < args.length; i++){
+            sources.add(Integer.parseInt(args[i]));
+        }
+        DirectedDFS reachable = new DirectedDFS(G, sources);
+        for (int v = 0; v < G.V(); v++){
+            if (reachable.marked(v)) {
+                StdOut.print(v + " ");
+            }
+        }
+        StdOut.println();
+    }
+}
